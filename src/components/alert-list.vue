1b3fb9519f83c56089c5c1d3fac91b28f01bc33d
Mike McGonagle <mike.mcgonagle@comcast.net>
2022-12-07 10:16:00 -0500

    add alert list view and feed from server

---
+++ src/components/alert-list.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="mei-list-title">
+    Alerts
+  </div>
+  <div class="mei-list-header row">
+    <div class="mei-incident-list-col4">Alert ID</div>
+    <div class="mei-incident-list-col1">Topic</div>
+    <div class="mei-incident-list-col2">District</div>
+    <div class="mei-incident-list-col3">Priority</div>
+    <div class="mei-incident-list-col5">Class</div>
+    <div class="mei-incident-list-col6">Alarm ID</div>
+    <div class="mei-incident-list-col7">Created</div>
+  </div>
+  <div class="mei-incident-list-datarow" v-for="el in alerts" v-bind:key="el.alertId">
+    <div class="mei-incident-list-col4" :class="{ modified: el.modified_flag }">
+      {{ el.alertId }}</div>
+    <div class="mei-incident-list-col1">{{ el.topic }}</div>
+    <div class="mei-incident-list-col2">{{ el.district }}</div>
+    <div class="mei-incident-list-col3">{{ el.priority }}</div>
+    <div class="mei-incident-list-col5">{{ el.alertClass }}</div>
+    <div class="mei-incident-list-col6">{{ el.alarmId }}</div>
+    <div class="mei-incident-list-col7">{{ dateFormat(el.alertDate) }}</div>
+  </div>
+</template>
+
+<script>
+import { DateTime } from 'luxon'
+export default {
+  name: 'alert-list',
+  data () {
+    return {
+      alert: {
+        alertId: '5001',
+        alertClass: 'Fire',
+        district: 'D1',
+        topic: 'Topic A',
+        priority: 4,
+        alertDate: '12/1/22 8:00',
+        alarmId: '9099',
+        site: 'Base',
+        housing: 0,
+        facility: 'Gym'
+      },
+      alerts: [
+        {
+          alertId: '5001',
+          alertClass: 'Fire',
+          district: 'D1',
+          topic: 'Topic A',
+          priority: 4,
+          alertDate: '12/1/22 8:00',
+          alarmId: '9099',
+          site: 'Base',
+          housing: 0,
+          facility: 'Gym',
+          modified_flag: false
+        },
+        {
+          alertId: '5002',
+          alertClass: 'EMT',
+          district: 'D6',
+          topic: 'Topic Z',
+          priority: 3,
+          alertDate: '12/1/22 9:00',
+          alarmId: '9090',
+          site: 'Base',
+          housing: 0,
+          facility: 'Gym'
+        }
+      ]
+    }
+  },
+  created () {
+    this.$axios.get('/api/Alerts')
+      .then(res => {
+        this.alerts = res.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    
+  },
+  methods: {
+    dateFormat: (el) => {
+      console.log(el)
+      return DateTime.fromISO(el).toLocaleString(DateTime.DATETIME_SHORT)
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.mei-list-title {
+  font-size: 3em;
+  font-weight: bolder;
+}
+
+.mei-list-header {
+  font-size: 1.2em;
+  font-weight: bold;
+  width: 100%;
+  display: flex;
+}
+.mei-incident-list-datarow {
+  display: flex;
+}
+.mei-incident-list-col1 {
+  width: 10%;
+}
+.mei-incident-list-col2 {
+  width: 20%;
+}
+.mei-incident-list-col3 {
+  width: 10%;
+}
+.mei-incident-list-col4 {
+  width: 10%;
+}
+.mei-incident-list-col5 {
+  width: 10%;
+}
+.mei-incident-list-col6 {
+  width: 10%;
+}
+.mei-incident-list-col7 {
+  width: 20%;
+}
+.modified {
+  animation: blink-animation 1s steps(5, start) infinite;
+  -webkit-animation: blink-animation 1s steps(5, start) infinite;
+}
+@keyframes blink-animation {
+  to {
+    visibility: hidden;
+  }
+}
+@-webkit-keyframes blink-animation {
+  to {
+    visibility: hidden;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,15 @@
 <template>
   <div>
-    <h1>
-      This totally looks like Stack Overflow
-      <button v-b-modal.addQuestionModal class="btn btn-primary mt-2 float-right">
-        <i class="fas fa-plus"/> Ask a question
-      </button>
-    </h1>
-    <ul class="list-group question-previews mt-4">
-      <question-preview
-        v-for="question in questions"
-        :key="question.id"
-        :question="question"
-        class="list-group-item list-group-item-action mb-3" />
-    </ul>
-    <add-question-modal @question-added="onQuestionAdded"/>
+    <AlertList></AlertList>
   </div>
 </template>
 
 <script>
-import QuestionPreview from '@/components/question-preview'
-import AddQuestionModal from '@/components/add-question-modal'
+import AlertList from '@/components/alert-list'
 
 export default {
   components: {
-    QuestionPreview,
-    AddQuestionModal
+    AlertList
   },
   data () {
     return {
